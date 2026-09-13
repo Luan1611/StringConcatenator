8fe4606a75fe7ff35f8ca553b9df780dacc1c06e
Luan1611 <luan_1611@hotmail.com>
2026-09-13 16:35:10 -0300

    feat: AddWordScreen basic layout done

--- app/src/main/java/br/edu/ifsp/scl/prdm/sc3029531/stringconcatenator/ui/composable/AddWordScreen.kt
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc3029531/stringconcatenator/ui/composable/AddWordScreen.kt
@@ -1,2 +1,67 @@
 package br.edu.ifsp.scl.prdm.sc3029531.stringconcatenator.ui.composable
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.edu.ifsp.scl.prdm.sc3029531.stringconcatenator.ui.theme.StringConcatenatorTheme
+
+@Composable
+fun AddWordScreen(
+    modifier: Modifier
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "String no estado atual"
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = "texto estático",
+            label = {Text(text = "Text to send")},
+            onValueChange = {}
+        )
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+
+            }
+        ) {
+            Text(text = "Concatenar")
+        }
+    }
+
+}
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+
+@Preview(
+    name = "Dark Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+
+@Composable
+fun AddWordScreenPreview() {
+    StringConcatenatorTheme {
+        Surface {
+            AddWordScreen(
+                modifier = Modifier
+            )
+        }
+    }
+}
